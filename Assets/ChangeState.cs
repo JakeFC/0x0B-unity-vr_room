8f54b1b37e9564f47fb3a44577758e9ff29e6bfe
JakeFC <jacobchavera@yahoo.com>
2022-01-05 23:04:18 -0800

    Basic teleportation, movement, and door animation

---
+++ Assets/ChangeState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeState : MonoBehaviour
+{
+    public Animator doorController;
+	public GameObject mainRoom;
+
+    public void Activate()
+	{
+		bool open = doorController.GetBool("character_nearby");
+		doorController.SetBool("character_nearby", !open);
+		foreach (Transform child in transform)
+		{
+			foreach (Transform baby in child)
+			{
+				baby.gameObject.layer = 0;
+			}
+			child.gameObject.layer = 0;
+		}
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Movement : MonoBehaviour
+{
+	public XRNode inputController;
+	public Camera cam;
+	public float speed = 2;
+	private Vector2 inputAxis;
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputController);
+		device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+    }
+
+	private void FixedUpdate()
+	{
+		Quaternion headYaw = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0);
+		Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
+
+		gameObject.transform.Translate(direction * Time.fixedDeltaTime * speed);
+
+		gameObject.transform.Translate(Vector3.up * -3f * Time.fixedDeltaTime);
+	}
+}
